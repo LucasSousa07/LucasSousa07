cdf0d2648ca6a7063a1241e5fdac36ed0cb7476c
LucasSousa07 <142699113+LucasSousa07@users.noreply.github.com>
2023-10-27 08:37:41 -0300

    Layouts
    Cabeçalho, navegação e rodapé do codigo

---
+++ layouts/header.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+
+<header class="main-header">
+
+    <nav class="main-header-content">
+
+        <div class="logo">
+            <a href="../index.php">TechJobs</a>
+        </div>
+
+        <nav class="menu">
+
+            <ul>
+                <li class="item-menu">
+                    <a href="../index.php">
+                        <span class="icon"><i class="bi bi-house-door"></i></span>
+                        <span class="txt-link">Home</span>
+                    </a>
+                </li>
+                <li class="item-menu">
+                    <a style="color: coral" href="/auth/index.php">
+                        <span class="icon"><i class="bi bi-box-arrow-in-right"></i></span>
+                        <span class="txt-link">Login</span>
+                    </a>
+                </li>
+
+
+            </ul>
+
+
+        </nav>
+    </nav>
+</header>
